2c477b9cdff1304a1049fc3e8061952fa8e269d9
CAKRA <kyuackerman30@gmail.com>
2023-07-08 15:37:43 +0700

    Last Update

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -1,94 +1,93 @@
  <!-- Sidebar -->
  <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon">
-            <i class="fas fa-building"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">Donquixote</div>
-    </a>
+     <!-- Sidebar - Brand -->
+     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard/">
+         <div class="sidebar-brand-icon">
+             <i class="fas fa-building"></i>
+         </div>
+         <div class="sidebar-brand-text mx-3">Donquixote</div>
+     </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+     <!-- Divider -->
+     <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item {{ Request::is('dashboard') ? 'active' : ''}}">
-        <a class="nav-link" href="/dashboard">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+     <!-- Nav Item - Dashboard -->
+     <li class="nav-item {{ Request::is('dashboard') ? 'active' : '' }}">
+         <a class="nav-link" href="/dashboard">
+             <i class="fas fa-fw fa-tachometer-alt"></i>
+             <span>Dashboard</span></a>
+     </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+     <!-- Divider -->
+     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Interface
-    </div>
+     <!-- Heading -->
+     <div class="sidebar-heading">
+         Interface
+     </div>
 
-    <!-- Nav Item - Page s Collapse Menu -->
-    <li class="nav-item {{ Request::is('dashboard/data*') ? 'active' : ''}}">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Data</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Inner</h6>
-                <a class="collapse-item" href="/dashboard/data/room">Room</a>
-                <a class="collapse-item" href="/dashboard/data/status">Status Room</a>
-                <a class="collapse-item" href="/dashboard/data/type">Type Room</a>
-            </div>
-        </div>
-    </li>
+     <!-- Nav Item - Page s Collapse Menu -->
+     <li class="nav-item {{ Request::is('dashboard/data*') ? 'active' : '' }}">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+             aria-expanded="true" aria-controls="collapseTwo">
+             <i class="fas fa-fw fa-cog"></i>
+             <span>Data</span>
+         </a>
+         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <h6 class="collapse-header">Inner</h6>
+                 <a class="collapse-item" href="/dashboard/data/room">Room</a>
+                 <a class="collapse-item" href="/dashboard/data/status">Status Room</a>
+                 <a class="collapse-item" href="/dashboard/data/type">Type Room</a>
+             </div>
+         </div>
+     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item {{ Request::is('dashboard/user*') ? 'active' :'' }}">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseuser"
-            aria-expanded="true" aria-controls="collapseuser">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>User</span>
-        </a>
-        <div id="collapseuser" class="collapse" aria-labelledby="headinguser"
-            data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Data</h6>
-                <a class="collapse-item" href="/dashboard/user">User</a>
-                {{-- <a class="collapse-item" href="user-border.html">Borders</a>
+     <!-- Nav Item - Utilities Collapse Menu -->
+     <li class="nav-item {{ Request::is('dashboard/user*') ? 'active' : '' }}">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseuser"
+             aria-expanded="true" aria-controls="collapseuser">
+             <i class="fas fa-fw fa-wrench"></i>
+             <span>User</span>
+         </a>
+         <div id="collapseuser" class="collapse" aria-labelledby="headinguser" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <h6 class="collapse-header">Data</h6>
+                 <a class="collapse-item" href="/dashboard/user">User</a>
+                 {{-- <a class="collapse-item" href="user-border.html">Borders</a>
                 <a class="collapse-item" href="user-animation.html">Animations</a>
                 <a class="collapse-item" href="user-other.html">Other</a> --}}
-            </div>
-        </div>
-    </li>
+             </div>
+         </div>
+     </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+     <!-- Divider -->
+     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
+     <!-- Heading -->
+     <div class="sidebar-heading">
+         Addons
+     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item {{ Request::is('dashboard/order*') ? 'active' : ''}}">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-            aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pesan</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Order now:</h6>
-                <a class="collapse-item" href="/dashboard/order">Order</a>
-                <h6 class="collapse-header">Data Transactions:</h6>
-                <a class="collapse-item" href="/dashboard/order/history-pay">History Payment</a>
+     <!-- Nav Item - Pages Collapse Menu -->
+     <li class="nav-item {{ Request::is('dashboard/order*') ? 'active' : '' }}">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+             aria-expanded="true" aria-controls="collapsePages">
+             <i class="fas fa-fw fa-folder"></i>
+             <span>Pesan</span>
+         </a>
+         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <h6 class="collapse-header">Order now:</h6>
+                 <a class="collapse-item" href="/dashboard/order">Order</a>
+                 <h6 class="collapse-header">Data Transactions:</h6>
+                 <a class="collapse-item" href="/dashboard/order/history-pay">History Payment</a>
 
-            </div>
-        </div>
-    </li>
-    {{-- <li class="nav-item">
+             </div>
+         </div>
+     </li>
+     {{-- <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
             aria-expanded="true" aria-controls="collapsePages">
             <i class="fas fa-fw fa-folder"></i>
@@ -108,16 +107,16 @@
         </div>
     </li> --}}
 
-    <!-- Nav Item - Charts -->
+     <!-- Nav Item - Charts -->
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+     <!-- Divider -->
+     <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-{{--
+     <!-- Sidebar Toggler (Sidebar) -->
+     <div class="text-center d-none d-md-inline">
+         <button class="rounded-circle border-0" id="sidebarToggle"></button>
+     </div>
+     {{--
     <!-- Sidebar Message -->
     <div class="sidebar-card d-none d-lg-flex">
         <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
@@ -125,5 +124,5 @@
         <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
     </div> --}}
 
-</ul>
-<!-- End of Sidebar -->
+ </ul>
+ <!-- End of Sidebar -->
