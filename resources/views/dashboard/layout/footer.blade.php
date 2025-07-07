c7e26b4fe6cbae97f519e9d5b3857fd910f0a23d
miqbaljaffar <iqbaljaffar1108@gmail.com>
2025-07-07 10:13:01 +0700

    modified:   resources/views/dashboard/layout/footer.blade.php

--- resources/views/dashboard/layout/footer.blade.php
+++ resources/views/dashboard/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Aurora Haven</span>
+            <span>Copyright &copy; Mohammad Iqbal Jaffar</span>
         </div>
     </div>
 </footer>
